6dca46629db85b41b894f1afd16a361d7de6cfe7
brunoao86 <brunoao86@gmail.com>
2017-08-12 17:16:23 -0300

    Add test for HomeController

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,3 +1,9 @@
-describe HomeController do
-  pending '#index'
+describe HomeController, type: :controller do
+  describe '#index' do
+    before { get :index }
+
+    it { expect(response).to render_template(:index) }
+
+    it { expect(response).to have_http_status(:ok) }
+  end
 end
