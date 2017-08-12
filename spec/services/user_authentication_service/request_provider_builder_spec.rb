0d1062f882dc43e7e1caa6729b5c562c96ceef05
brunoao86 <brunoao86@gmail.com>
2017-08-12 15:28:27 -0300

    Add test for RequestProviderBuilder

--- spec/services/user_authentication_service/request_provider_builder_spec.rb
+++ spec/services/user_authentication_service/request_provider_builder_spec.rb
@@ -1,3 +1,37 @@
 describe UserAuthenticationService::RequestProviderBuilder do
-  pending '.build'
+  let(:raw_request) do
+    {
+      'provider' => provider
+    }
+  end
+
+  describe '.build' do
+    context 'when it is a google request' do
+      let(:provider) { 'google_oauth2' }
+      let(:request_google) { double('request_google') }
+
+      before do
+        allow(UserAuthenticationService::RequestGoogle).to receive(:new)
+          .with(raw_request).and_return(request_google)
+      end
+
+      it 'initializes RequestGoogle with raw_request' do
+        expect(described_class.build(raw_request)).to eq(request_google)
+      end
+    end
+
+    context 'when it is a facebook request' do
+      let(:provider) { 'facebook' }
+      let(:request_facebook) { double('request_facebook') }
+
+      before do
+        allow(UserAuthenticationService::RequestFacebook).to receive(:new)
+          .with(raw_request).and_return(request_facebook)
+      end
+
+      it 'initializes RequestFacebook with raw_request' do
+        expect(described_class.build(raw_request)).to eq(request_facebook)
+      end
+    end
+  end
 end
