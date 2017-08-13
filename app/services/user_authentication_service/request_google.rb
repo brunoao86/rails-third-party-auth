1440c1f40208646e5322260f5d84c356eccbb56b
brunoao86 <brunoao86@gmail.com>
2017-08-12 21:59:20 -0300

    RequestGoogle refactor

--- app/services/user_authentication_service/request_google.rb
+++ app/services/user_authentication_service/request_google.rb
@@ -1,20 +1,55 @@
 class UserAuthenticationService::RequestGoogle < UserAuthenticationService::RequestBase
-  def initialize(raw_request)
-    google_request = raw_request.env["omniauth.auth"]
+  attr_reader :request_auth
 
-    @provider                 = 'google'
-    @uid                      = google_request.uid
-    @user_name                = google_request.info.name
-    @user_gender              = google_request.extra.raw_info.gender
-    @user_email               = google_request.info.email
-    @user_image               = google_request.info.image
-    @user_locale              = google_request.extra.raw_info.locale
-    @credentials_token        = google_request.credentials.token
-    @credentials_expiration   = Time.at(google_request.credentials.expires_at)
-    @request_validation_token = google_request.extra.id_info.aud
+  def initialize(raw_request)
+    @request_auth = raw_request.env["omniauth.auth"]
+  end
+
+  def provider
+    'google'
+  end
+
+  def uid
+    @uid ||= request_auth.uid
+  end
+
+  def user_name
+    @user_name ||= request_auth.info.name
+  end
+
+  def user_gender
+    @user_gender ||= request_auth.extra.raw_info.gender
+  end
+
+  def user_email
+    @user_email ||= request_auth.info.email
+  end
+
+  def raw_image_url
+    @raw_image_url ||= request_auth.info.image
+  end
+
+  def user_image_url
+    "#{raw_image_url}?size=200" if raw_image_url.present?
+  end
+
+  def user_locale
+    @user_locale ||= request_auth.extra.raw_info.locale
+  end
+
+  def credentials_token
+    @credentials_token ||= request_auth.credentials.token
+  end
+
+  def credentials_expiration
+    @credentials_expiration ||= Time.at(request_auth.credentials.expires_at)
+  end
+
+  def request_validation_token
+    @request_validation_token ||= request_auth.extra.id_info.aud
   end
 
   def valid?
-    @request_validation_token === ENV['GOOGLE_CLIENT_ID']
+    request_validation_token === ENV['GOOGLE_CLIENT_ID']
   end
 end
--- spec/services/user_authentication_service/request_google_spec.rb
+++ spec/services/user_authentication_service/request_google_spec.rb
@@ -1,94 +1,127 @@
 describe UserAuthenticationService::RequestGoogle do
   let(:raw_request) { spy('raw_request', env: raw_request_env) }
+
   let(:raw_request_env) do
     {
-      "omniauth.auth" => google_response_obj
+      "omniauth.auth" => request_auth
     }
   end
-  let(:google_response_obj) do
-    JSON.parse(google_response.to_json, object_class: OpenStruct)
-  end
-  let(:google_response) do
-    {
-      uid: 'dummy uid',
-      info: {
-        name: 'dummy name',
-        email: 'dummy email',
-        image: 'dummy image',
-      },
-      extra: {
-        raw_info: {
-          gender: 'dummy gender',
-          locale: 'dummy locale'
-        },
-        id_info: {
-          aud: dummy_request_token_value
-        }
-      },
-      credentials: {
-        token: 'dummy token',
-        expires_at: 1501690375
-      }
-    }
-  end
-  let(:dummy_request_token_value) { 'dUmmy_r3qu3st_t0k3n' }
+
+  let(:request_auth) { double('request_auth') }
 
   subject { described_class.new(raw_request) }
 
   it { expect(subject).to be_a_kind_of(UserAuthenticationService::RequestBase) }
 
   describe 'initialization' do
-    describe 'provider' do
-      it { expect(subject.instance_variable_get(:@provider)).to eq('google') }
+    describe 'request_auth' do
+      it { expect(subject.instance_variable_get(:@request_auth)).to eq(request_auth) }
+    end
+  end
+
+  describe '#request_auth' do
+    it { expect(subject.request_auth).to eq(request_auth) }
+  end
+
+  describe '#provider' do
+    it { expect(subject.provider).to eq('google') }
+  end
+
+  describe 'dependents from request_auth' do
+    let(:request_auth_obj) do
+      JSON.parse(request_auth.to_json, object_class: OpenStruct)
     end
 
-    describe 'uid' do
-      it { expect(subject.instance_variable_get(:@uid)).to eq('dummy uid') }
+    let(:request_auth) do
+      {
+        uid: 'dummy uid',
+        info: {
+          name: 'dummy name',
+          email: 'dummy email',
+          image: 'dummy_raw_image_url',
+        },
+        extra: {
+          raw_info: {
+            gender: 'dummy gender',
+            locale: 'dummy locale'
+          },
+          id_info: {
+            aud: dummy_request_token_value
+          }
+        },
+        credentials: {
+          token: 'dummy token',
+          expires_at: 1501690375
+        }
+      }
     end
 
-    describe 'user_name' do
-      it { expect(subject.instance_variable_get(:@user_name)).to eq('dummy name') }
+    let(:dummy_request_token_value) { 'dUmmy_r3qu3st_t0k3n' }
+
+    before { allow(subject).to receive(:request_auth).and_return(request_auth_obj) }
+
+    describe '#uid' do
+      it { expect(subject.uid).to eq('dummy uid') }
     end
 
-    describe 'user_gender' do
-      it { expect(subject.instance_variable_get(:@user_gender)).to eq('dummy gender') }
+    describe '#user_name' do
+      it { expect(subject.user_name).to eq('dummy name') }
     end
 
-    describe 'user_email' do
-      it { expect(subject.instance_variable_get(:@user_email)).to eq('dummy email') }
+    describe '#user_gender' do
+      it { expect(subject.user_gender).to eq('dummy gender') }
     end
 
-    describe 'user_image' do
-      it { expect(subject.instance_variable_get(:@user_image)).to eq('dummy image') }
+    describe '#user_email' do
+      it { expect(subject.user_email).to eq('dummy email') }
     end
 
-    describe 'user_locale' do
-      it { expect(subject.instance_variable_get(:@user_locale)).to eq('dummy locale') }
+    describe '#raw_image_url' do
+      it { expect(subject.raw_image_url).to eq('dummy_raw_image_url') }
     end
 
-    describe 'credentials_token' do
+    describe '#user_locale' do
+      it { expect(subject.user_locale).to eq('dummy locale') }
+    end
+
+    describe '#credentials_token' do
+      it { expect(subject.credentials_token).to eq('dummy token') }
+    end
+
+    describe '#credentials_expiration' do
       it do
-        expect(subject.instance_variable_get(:@credentials_token)).to eq('dummy token')
+        expect(subject.credentials_expiration).to eq(Time.at(1501690375))
       end
     end
 
-    describe 'credentials_expiration' do
+    describe '#request_validation_token' do
       it do
-        expected = Time.at(1501690375)
-        expect(subject.instance_variable_get(:@credentials_expiration)).to eq(expected)
-      end
-    end
-
-    describe 'request_validation_token' do
-      it do
-        expected = 'dUmmy_r3qu3st_t0k3n'
-        expect(subject.instance_variable_get(:@request_validation_token)).to eq(expected)
+        expect(subject.request_validation_token).to eq('dUmmy_r3qu3st_t0k3n')
       end
     end
   end
 
+  describe '#user_image_url' do
+    before { allow(subject).to receive(:raw_image_url).and_return(raw_image_url) }
+
+    context 'when raw_image_url is defined' do
+      let(:raw_image_url) { 'http://dummy_raw_url' }
+
+      it { expect(subject.user_image_url).to eq('http://dummy_raw_url?size=200') }
+    end
+
+    context 'when raw_image_url is NOT defined' do
+      let(:raw_image_url) { '' }
+
+      it { expect(subject.user_image_url).to be_nil }
+    end
+  end
+
   describe '#valid?' do
+    let(:dummy_token) { 'dUmmy_r3qu3st_t0k3n' }
+
     before do
+      allow(subject).to receive(:request_validation_token).and_return(dummy_token)
       ENV['GOOGLE_CLIENT_ID'] = 'dUmmy_r3qu3st_t0k3n'
     end
 
@@ -97,7 +130,7 @@ describe UserAuthenticationService::RequestGoogle do
     end
 
     context 'when request validation token is NOT equal to the environment' do
-      let(:dummy_request_token_value) { 'd1ff3r3nt_dUmmy_r3qu3st_t0k3n' }
+      let(:dummy_token) { 'd1ff3r3nt_dUmmy_r3qu3st_t0k3n' }
 
       it { expect(subject.valid?).to eq(false) }
     end
