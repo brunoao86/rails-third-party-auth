c58893d8c0def5cd73354f53d08bac014f3c3fef
brunoao86 <brunoao86@gmail.com>
2017-09-09 09:43:18 -0300

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  resources :sessions, only: [:create, :destroy]
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'login', to: 'sessions#new', as: 'login'
+
+  resources :sessions, only: [:new, :create, :destroy]
   resource :home, only: [:index]
 
   root to: 'home#index'
