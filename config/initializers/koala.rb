1257f325f50a1a30c10f719ca4548da15504deec
brunoao86 <brunoao86@gmail.com>
2017-09-10 19:58:47 -0300

    Remove dummy ids to avoid misunderstands

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,5 +1,5 @@
 Koala.configure do |config|
   config.api_version = 'v2.10'
-  config.app_id = ENV.fetch('FACEBOOK_CLIENT_ID') { 'DUMMY_FACEBOOK_CLIENT_ID' }
-  config.app_secret = ENV.fetch('FACEBOOK_SECRET_ID') { 'DUMMY_FACEBOOK_SECRET_ID' }
+  config.app_id = ENV.fetch('FACEBOOK_CLIENT_ID')
+  config.app_secret = ENV.fetch('FACEBOOK_SECRET_ID')
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-    ENV.fetch('GOOGLE_CLIENT_ID') { 'DUMMY_GOOGLE_CLIENT_ID' },
-    ENV.fetch('GOOGLE_SECRET_ID') { 'DUMMY_GOOGLE_SECRET_ID' },
+    ENV.fetch('GOOGLE_CLIENT_ID'),
+    ENV.fetch('GOOGLE_SECRET_ID'),
     {
       client_options:
       {
@@ -14,7 +14,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     }
 
   provider :facebook,
-    ENV.fetch('FACEBOOK_CLIENT_ID') { 'DUMMY_FACEBOOK_CLIENT_ID' },
-    ENV.fetch('FACEBOOK_SECRET_ID') { 'DUMMY_FACEBOOK_SECRET_ID' },
+    ENV.fetch('FACEBOOK_CLIENT_ID'),
+    ENV.fetch('FACEBOOK_SECRET_ID'),
     scope: 'email,user_about_me'
 end
