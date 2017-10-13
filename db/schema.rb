7436615fdb70556ca127a39189f721d09b1ecd68
Joseph Gilgen <gilgenlabs@gmail.com>
2017-10-13 14:02:51 -0700

    fixed accidental delete from schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170812211210) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
